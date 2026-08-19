20f5f66e7b440c71d21bdf0655be17b2c07cf6b6
fwo912594-ctrl <fwo912594@gmail.com>
2026-08-19 18:43:06 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.LIFE_LAB_CONFIG = {
   supabaseUrl: "https://uplgoigxbhvlzsfqtzna.supabase.co",
-  supabasePublishableKey: "这里粘贴你的Publishable key"
+  supabasePublishableKey: "sb_publishable_B47f5mO14jrK22ogC9wuuQ_QOfFqhP0"
 };
